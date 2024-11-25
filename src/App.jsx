df6468d1ab4f053687bc0af9129704b69a58a116
Nurunnahar <nurunnahar9081@gmail.com>
2024-11-25 20:59:43 +0600

    html setup

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,10 @@ import SingleProduct from "./components/product/SingleProduct";
 import Cart from "./pages/ecom/Cart";
 import Checkout from "./pages/ecom/Checkout";
 import Dashboard from "./pages/admin/Dashboard";
+import Brand from "./pages/admin/Brand";
+import Tag from "./pages/admin/Tag";
+import Category from "./pages/admin/Category";
+import CreateProduct from "./pages/admin/CreateProduct";
 
 function App() {
   return (
@@ -17,7 +21,12 @@ function App() {
         <Route path="/:slug" element={<SingleProduct />}></Route>
         <Route path="/cart" element={<Cart />}></Route>
         <Route path="/checkout" element={<Checkout />}></Route>
-        <Route path="/dashboard" element={<Dashboard />}></Route>
+        <Route path="/dashboard" element={<Dashboard />}>
+          <Route path="brand" element={<Brand />} />
+          <Route path="tag" element={<Tag />} />
+          <Route path="category" element={<Category />} />
+          <Route path="createproduct" element={<CreateProduct />} />
+        </Route>
       </Routes>
       <Footer />
     </>
--- src/pages/admin/Dashboard.jsx
+++ src/pages/admin/Dashboard.jsx
@@ -9,73 +9,34 @@ const Dashboard = () => {
         <div className="container">
           <div className="section-content">
             <div className="row">
-              <div className="col-md-8 col-md-offset-2">
-                <div className="title center">
-                  <h3 className="fw-400 mb-50">Minimal </h3>
-                </div>
-                <ul role="tablist" className="admin-tools nav nav-tabs outline">
-                  <li role="presentation" >
-                    <a href="#">Product </a>
+              <div className="col-md-3">
+                <ul role="tablist" className="admin-tabs nav nav-tabs outline">
+                  <li role="presentation" className="active">
+                    <a href="#first-tab-2" role="tab" data-toggle="tab">
+                      First
+                    </a>
                   </li>
-                  <li role="presentation"  >
-                    <a href="#">Product </a>
+                  <li role="presentation">
+                    <a href="#second-tab-2" role="tab" data-toggle="tab">
+                      Second
+                    </a>
                   </li>
-                  <li role="presentation" >
-                    <a href="#">Product </a>
-                  </li>
-                  <li role="presentation"  >
-                    <a href="#">Product </a>
-                  </li>
-                  <li role="presentation" >
-                    <a href="#">Product </a>
+                  <li role="presentation">
+                    <a href="#third-tab-2" role="tab" data-toggle="tab">
+                      Third
+                    </a>
                   </li>
                 </ul>
-                <div className="tab-content">
-                  <div
-                    id="first-tab-2"
-                    role="tabpanel"
-                    className="tab-pane active"
-                  >
-                    <p>
-                      Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                      Illo facilis ab impedit ipsam inventore, odio sit
-                      repudiandae. Fuga soluta officia quas voluptatem, dolores
-                      nulla eaque nostrum nobis culpa illum rerum. Ut molestias
-                      nemo voluptas velit explicabo dolores iusto labore eius,
-                      quidem voluptates praesentium, libero minus cumque aut
-                      consequatur expedita excepturi quae voluptatum.
-                    </p>
-                  </div>
-                  <div id="second-tab-2" role="tabpanel" className="tab-pane">
-                    <p>
-                      Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                      Quo vel qui cumque laborum asperiores, magnam, deserunt,
-                      impedit eveniet dolores non placeat aut recusandae.
-                      Maxime, cupiditate. Similique obcaecati aliquid amet odio.
-                      Magnam perferendis tenetur temporibus adipisci. Adipisci,
-                      debitis sit veniam possimus sed, natus. Molestias possimus
-                      rerum laborum quasi, voluptas quidem assumenda sapiente
-                      vero.
-                    </p>
-                  </div>
-                  <div id="third-tab-2" role="tabpanel" className="tab-pane">
-                    <p>
-                      Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                      Alias blanditiis porro nobis non qui, excepturi deserunt
-                      fugiat est quos vitae vero laboriosam hic quis, aliquam
-                      tempore odio sint, distinctio libero. Officia sit esse
-                      dolorum excepturi perferendis? Voluptatibus iste a illum
-                      dolorem nam odit sequi reiciendis explicabo ullam pariatur
-                      facilis eos consequatur, perspiciatis!
-                    </p>
-                  </div>
-                </div>
+              </div>
+              <div className="col-md-9">Content</div>
+               
+         
               </div>
             </div>
             {/* end of row*/}
           </div>
           {/* end of section content*/}
-        </div>
+        
         {/* end of container*/}
       </section>
     </>
